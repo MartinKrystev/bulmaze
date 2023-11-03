f82f056daae8d5c757c7f38a442a6278c478fbee
MartinKrystev <martin.krystev@gmail.com>
2023-11-03 16:40:54 +0200

    Repos.

--- src/main/java/com/project/bulmaze/repository/AnswerRepository.java
+++ src/main/java/com/project/bulmaze/repository/AnswerRepository.java
@@ -4,6 +4,9 @@ import com.project.bulmaze.model.entity.AnswerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
+    Optional<AnswerEntity> findByName(String name);
 }
--- src/main/java/com/project/bulmaze/repository/QuestionRepository.java
+++ src/main/java/com/project/bulmaze/repository/QuestionRepository.java
@@ -4,6 +4,9 @@ import com.project.bulmaze.model.entity.QuestionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<QuestionEntity, Long> {
+    Optional<QuestionEntity> findByName(String name);
 }
