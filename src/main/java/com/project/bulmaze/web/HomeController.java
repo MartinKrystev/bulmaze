b6105fef4179e6bc3972c2950115fe00b81d544d
MartinKrystev <martin.krystev@gmail.com>
2023-11-16 11:20:03 +0200

    Home Controller.

--- src/main/java/com/project/bulmaze/web/HomeController.java
+++ src/main/java/com/project/bulmaze/web/HomeController.java
@@ -2,32 +2,23 @@ package com.project.bulmaze.web;
 
 import com.project.bulmaze.model.dto.AddInquiryDTO;
 import com.project.bulmaze.model.dto.NewsletterDTO;
-import com.project.bulmaze.model.entity.FaqEntity;
-import com.project.bulmaze.service.FaqService;
 import com.project.bulmaze.service.InquiryService;
 import com.project.bulmaze.service.NewsletterService;
-import com.project.bulmaze.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
-
 @Controller
 public class HomeController {
-    private final FaqService faqService;
-    private final UserService userService;
+
     private final NewsletterService newsletterService;
     private final InquiryService inquiryService;
 
-    public HomeController(FaqService faqService, UserService userService, NewsletterService newsletterService, InquiryService inquiryService) {
-        this.faqService = faqService;
-        this.userService = userService;
+    public HomeController(NewsletterService newsletterService, InquiryService inquiryService) {
         this.newsletterService = newsletterService;
         this.inquiryService = inquiryService;
     }
@@ -55,17 +46,9 @@ public class HomeController {
         return "index";
     }
 
-
     @GetMapping("/read-more")
     public String readMore() {
         return "read-more";
-    }
-
-    @GetMapping("/faq")
-    public String getFAQ(Model model) {
-        List<FaqEntity> allFAQs = this.faqService.allFaqs();
-        model.addAttribute("allFAQs", allFAQs);
-        return "faq";
     }
 
     @GetMapping("/contact")
