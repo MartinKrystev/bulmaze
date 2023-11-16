38f9ce4ca29aa8a342be735f3b6ae875acac8a86
MartinKrystev <martin.krystev@gmail.com>
2023-11-16 21:13:40 +0200

    PlayController.

--- src/main/java/com/project/bulmaze/web/PlayController.java
+++ src/main/java/com/project/bulmaze/web/PlayController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -22,12 +23,21 @@ public class PlayController {
     private final OptionsService optionsService;
     private final ClueService clueService;
 
-    public PlayController(QuestionService questionService, AnswerService answerService, UserService userService, OptionsService optionsService, ClueService clueService) {
+    public PlayController(QuestionService questionService,
+                          AnswerService answerService,
+                          UserService userService,
+                          OptionsService optionsService,
+                          ClueService clueService) {
         this.questionService = questionService;
         this.answerService = answerService;
         this.userService = userService;
         this.optionsService = optionsService;
         this.clueService = clueService;
+    }
+
+    @GetMapping("/play{userProgress}")
+    public String getTEST(@PathVariable Long userProgress) {
+        return "redirect:/play";
     }
 
     @GetMapping("/play")
@@ -36,14 +46,13 @@ public class PlayController {
         List<QuestionDTO> allQuestions = this.questionService.allQuestions();
 
         if (userDTO.getUserProgress() < allQuestions.size()) {
-
             QuestionDTO questionDTO = this.questionService.askNextQuestion(userDTO.getUserProgress() + 1, principal);
             OptionsDTO optionsDTO = this.optionsService.optionsCurrQuestion(userDTO.getUserProgress() + 1);
             ClueDTO clueDTO = this.clueService.getClue(userDTO.getUserProgress() + 1);
 
             model.addAttribute("user", userDTO);
-            model.addAttribute("question", questionDTO);
             model.addAttribute("allQuestions", allQuestions);
+            model.addAttribute("question", questionDTO); //TODO: updates userProgress
             model.addAttribute("options", optionsDTO);
             model.addAttribute("clueDTO", clueDTO);
         } else {
@@ -59,19 +68,21 @@ public class PlayController {
                                Principal principal) {
 
         if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("answerDTO", answerDTO)
+                    .addFlashAttribute("org.springframework.validation.BindingResult.answerDTO", bindingResult);
             return "redirect:/500";
         }
 
         UserDTO user = this.userService.findByUsername(principal.getName());
-        AnswerDTO answer = this.answerService.getAnswer(user.getUserProgress());
+        AnswerDTO answer = this.answerService.getAnswer(user.getUserProgress() + 1);
 
         if (this.answerService.compareAnswers(answer.getDescription(), answerDTO.getDescription())) {
-            this.userService.addCorrectAnswer(user, answer);
+            this.userService.addCorrectAnswer(user, answerDTO);
+        } else {
+            this.userService.addWrongAnswer(user, answerDTO);
         }
-
         return "redirect:/play";
     }
-
 
     //Model Attributes
     @ModelAttribute(name = "answerDTO")
