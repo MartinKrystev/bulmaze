bbe8d7f4802fd8c1fda2ee98fe56c4d4d9ac4f39
MartinKrystev <martin.krystev@gmail.com>
2023-11-16 21:12:05 +0200

    AnswerService.

--- src/main/java/com/project/bulmaze/service/AnswerService.java
+++ src/main/java/com/project/bulmaze/service/AnswerService.java
@@ -1,11 +1,11 @@
 package com.project.bulmaze.service;
 
 import com.project.bulmaze.model.dto.AnswerDTO;
-import com.project.bulmaze.model.dto.FirstAnswerDTO;
+
+import java.util.List;
 
 public interface AnswerService {
-    boolean firstAnswer(FirstAnswerDTO firstAnswerDTO);
     AnswerDTO getAnswer(Long id);
-
     boolean compareAnswers(String correctAnswer, String givenAnswer);
+    List<AnswerDTO> getAllGivenAnswers(String name);
 }
