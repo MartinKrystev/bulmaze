23d7e12a00a8bbd1534670edc355d6826c260d4a
MartinKrystev <martin.krystev@gmail.com>
2023-11-23 12:39:21 +0200

    InquiryService

--- src/main/java/com/project/bulmaze/service/InquiryService.java
+++ src/main/java/com/project/bulmaze/service/InquiryService.java
@@ -1,11 +1,9 @@
 package com.project.bulmaze.service;
 
 import com.project.bulmaze.model.dto.AddInquiryDTO;
-import com.project.bulmaze.model.entity.InquiryEntity;
 
-import java.util.Optional;
 
 public interface InquiryService {
     boolean addInquiry(AddInquiryDTO addInquiry);
-    Optional<InquiryEntity> findByEmailAndSubject(String email, String subject);
+    void sendInquiryMails();
 }
