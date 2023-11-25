1be24cef7367411d25fc0a1f1fae2b08a5c8caf6
MartinKrystev <martin.krystev@gmail.com>
2023-11-25 09:58:20 +0200

    ClueService

--- src/main/java/com/project/bulmaze/service/ClueService.java
+++ src/main/java/com/project/bulmaze/service/ClueService.java
@@ -2,6 +2,9 @@ package com.project.bulmaze.service;
 
 import com.project.bulmaze.model.dto.ClueDTO;
 
+import java.util.List;
+
 public interface ClueService {
     ClueDTO getClue(Long id);
+    List<ClueDTO> getAllClues();
 }
