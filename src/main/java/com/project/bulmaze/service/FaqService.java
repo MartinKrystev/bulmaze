a285d4422fcc4bef1642823f42d76d24d4a902c6
MartinKrystev <martin.krystev@gmail.com>
2023-12-09 20:41:35 +0200

    Updates

--- src/main/java/com/project/bulmaze/service/FaqService.java
+++ src/main/java/com/project/bulmaze/service/FaqService.java
@@ -1,5 +1,6 @@
 package com.project.bulmaze.service;
 
+import com.project.bulmaze.model.dto.AddFaqDTO;
 import com.project.bulmaze.model.dto.EditFaqDTO;
 import com.project.bulmaze.model.entity.FaqEntity;
 
@@ -10,4 +11,5 @@ public interface FaqService {
     EditFaqDTO findById(Long id);
     boolean saveEditedFAQ(EditFaqDTO editFaqDTO);
     void deleteFAQ(Long id);
+    boolean addNewFaq(AddFaqDTO addFaqDTO);
 }
